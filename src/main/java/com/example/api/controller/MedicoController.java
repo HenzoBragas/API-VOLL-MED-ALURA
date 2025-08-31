5e4b62be2e3ff311c6595f092cbdab08c5533003
Henzo Bragas <henzo.bragas@hotmail.com>
2025-08-31 14:00:31 -0300

    feat: paginação metodo GET de medicos

--- src/main/java/com/example/api/controller/MedicoController.java
+++ src/main/java/com/example/api/controller/MedicoController.java
@@ -2,17 +2,18 @@ package com.example.api.controller;
 
 
 import com.example.api.dto.DadosCadastroMedico;
+import com.example.api.dto.DadosListagemMedico;
 import com.example.api.model.Medico;
 import com.example.api.repository.MedicoRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -29,6 +30,13 @@ public class MedicoController {
     public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         medicoRepository.save(new Medico(dados));
         return ResponseEntity.status(HttpStatus.CREATED).body(dados);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemMedico>> listar(Pageable paginacao) {
+        Page<DadosListagemMedico> medicos = medicoRepository.findAll(paginacao)
+                .map(DadosListagemMedico::new);
+        return ResponseEntity.status(HttpStatus.OK).body(medicos);
     }
 
 }
