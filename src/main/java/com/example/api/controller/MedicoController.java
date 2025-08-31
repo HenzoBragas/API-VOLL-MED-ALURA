01b61c642f2a2f1192b5c1d423b676786679568a
Henzo Bragas <henzo.bragas@hotmail.com>
2025-08-31 15:39:16 -0300

    ordenação via requisição e spring

--- src/main/java/com/example/api/controller/MedicoController.java
+++ src/main/java/com/example/api/controller/MedicoController.java
@@ -9,6 +9,7 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -33,7 +34,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<DadosListagemMedico>> listar(Pageable paginacao) {
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         Page<DadosListagemMedico> medicos = medicoRepository.findAll(paginacao)
                 .map(DadosListagemMedico::new);
         return ResponseEntity.status(HttpStatus.OK).body(medicos);
